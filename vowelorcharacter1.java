702389c83dac0ffd88c8d842c9f2af6eb4f86fc8
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-08-09 22:56:21 +0530

    finding the char is vowel or consonent

---
+++ vowelorcharacter1.java
@@ -0,0 +1,13 @@
+public class vowelorcharacter1 {
+    public static void main(String[] args) {
+        char c='f';
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        {
+            System.out.println(c + " is a vowel ");  // condition true input is vowel
+        }
+        else
+        {
+            System.out.println(c + " is a consonant ");  // condition true input is consonant
+        }
+    }
+}
