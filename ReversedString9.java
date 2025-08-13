20d8f1abb4f882acde33f9492cd1fe7278514435
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-08-13 18:40:31 +0530

    reversed the string  using stringbuilder

---
+++ ReversedString9.java
@@ -0,0 +1,8 @@
+public class ReversedString9 {
+    public static void main(String[] args) {
+        String sc="ramabhi";
+        StringBuilder sb=new StringBuilder(sc);
+        String reversed=sb.reverse().toString();
+        System.out.println(reversed);
+    }
+}
